7dfa03dc43c8a745370661ecd6be3535b33325b2
Abdessadek <abdessadekhm@gmail.com>
2026-02-04 10:32:35 +0100

    Add estate property initial staff

---
+++ estate/models/estate_property.py
@@ -0,0 +1,46 @@
+from odoo import models, fields
+
+class Property(models.Model):
+    _name = "estate.property"
+    _description="easy peasy module"
+
+    name=fields.Char(required=True)
+    description=fields.Text()
+    postcode=fields.Char()
+    date_availability=fields.Date(copy=False, default=fields.Datetime.add(fields.Datetime.today(),months=3))
+    expected_price=fields.Float(required=True)
+    selling_price=fields.Float(readonly=True,copy=False)
+    bedrooms=fields.Integer(default=2)
+    living_area=fields.Integer()
+    facades=fields.Integer()
+    garage=fields.Boolean()
+    garden=fields.Boolean()
+    gareden_area=fields.Integer()
+    garden_orientation=fields.Selection([
+        ('north', 'North'),
+        ('west', 'West'),
+        ('east', 'East'),
+        ('south', 'South')
+    ], default='north')
+    active=fields.Boolean()
+    state=fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled'),
+
+    ], default='new')
+    test=fields.Selection([
+        ('test', 'test'),
+        ('test1', 'test1'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled'),
+
+    ])
+
+
+    
+    
+    pass
