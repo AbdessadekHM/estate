b11c1c24dc3a6279af82e07d45620cb94b89af3c
Abdessadek <abdessadekhm@gmail.com>
2026-02-05 10:32:12 +0100

    [UPD] update estate property model and views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Property(models.Model):
     _name = "estate.property"
@@ -10,18 +10,18 @@ class Property(models.Model):
     date_availability=fields.Date(copy=False, default=fields.Datetime.add(fields.Datetime.today(),months=3), string="Available From")
     expected_price=fields.Float(required=True)
     selling_price=fields.Float(readonly=True,copy=False)
-    bedrooms=fields.Integer(default=2)
-    living_area=fields.Integer()
+    bedrooms=fields.Integer(default=2, string="Bedrooms")
+    living_area=fields.Integer(string="Living Area (sqm)")
     facades=fields.Integer()
-    garage=fields.Boolean()
-    garden=fields.Boolean()
-    gareden_area=fields.Integer()
+    garage=fields.Boolean(string="Garage")
+    garden=fields.Boolean(string="Garden")
+    garden_area=fields.Integer(string="Garden Area (sqm)")
     garden_orientation=fields.Selection([
         ('north', 'North'),
         ('west', 'West'),
         ('east', 'East'),
         ('south', 'South')
-    ], default='north')
+    ], default='north',string="Garden Orientation")
     active=fields.Boolean()
     state=fields.Selection([
         ('new', 'New'),
@@ -37,6 +37,22 @@ class Property(models.Model):
     buyer=fields.Many2one("res.partner", copy=False)
     tag_ids=fields.Many2many("estate.property.tag")
     offer_ids=fields.One2many("estate.property.offer", "partner_id", string="Offers")
+    total_area=fields.Integer(compute="_calculate_total_area", string="Total Area")
+    best_price=fields.Float(compute="_calculate_best_price", string="Best Price")
+
+
+
+    @api.depends("garden_area","living_area")
+    def _calculate_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+    
+    def _calculate_best_price(self):
+
+        for record in self:
+            record.best_price = max(record.mapped("offer_ids.price"))
+
+        pass
 
 
     # def action_do_something(self):
