e508b61b688c7fc420c580431178985b0e12058f
Abdessadek <abdessadekhm@gmail.com>
2026-02-05 11:47:32 +0100

    [UPD] Add compute fields to the offer model and improve the view UI

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 
@@ -15,5 +15,27 @@ class Offer(models.Model):
 
     partner_id=fields.Many2one("res.partner", string="Partner ID", required=True)
     property_id=fields.Many2one("estate.property", string="Estate Property", required=True)
+
+    validity=fields.Integer(default=7, string="Validity")
+    date_deadline=fields.Date(compute="_calculate_date_deadline", inverse="_inverse_deadline")
+
+    @api.depends("validity", "create_date")
+    def _calculate_date_deadline(self):
+        print("calculate deadline")
+        for record in self:
+            record.date_deadline = fields.Datetime.add(record.create_date,days=record.validity)
+
+
+
+    def _inverse_deadline(self):
+
+        print("check_validity")
+        for record in self:
+            if record.date_deadline:
+                record.validity = (fields.Date.to_date(record.date_deadline) - fields.Date.to_date(record.create_date)).days 
+
+
+
+            
     
 
