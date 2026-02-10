9c72cd1029883ab90238b64494b60d140647575c
Abdessadek <abdessadekhm@gmail.com>
2026-02-10 10:46:54 +0100

    [ADD] estate: create a server action for state check

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,12 +5,14 @@
     'data': [
        "security/security.xml",
        "security/ir.model.access.csv",
+       "data/ir_actions_server_data.xml",
         "views/estate_property_offer_views.xml",
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
         "views/estate_menus.xml",
         "views/res_users_views.xml"
+
     ],
 
 }
